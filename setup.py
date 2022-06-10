e6322b1da1c7108f1f58c5619cfe34df1b5546d5
Inbal Paz <inbal.paz@tuebingen.mpg.de>
2022-06-10 12:53:23 +0200

    Added LICENSE.txt, setup.py, meta.yaml

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='CLANS',
+    version='v2.0.1_beta',
+    packages=['clans', 'clans.io', 'clans.io.file_formats', 'clans.GUI', 'clans.layouts', 'clans.graphics',
+              'clans.taxonomy', 'clans.similarity_search'],
+    url='https://github.com/inbalpaz/CLANS',
+    license='GPL',
+    author='Inbal Paz',
+    author_email='inbal.paz@tuebingen.mpg.de',
+    description='Program for visualizing the relationship between proteins based on their pairwise sequence similarities.'
+)
